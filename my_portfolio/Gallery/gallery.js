8ffdb77bd35e7ee0c8232507c8e48c1be7b521d2
ithachioji2_546 <ithachioji21.546@gmail.com>
2026-02-03 15:53:05 +0900

    nav back worked

--- my_portfolio/Gallery/gallery.js
+++ my_portfolio/Gallery/gallery.js
@@ -5,20 +5,23 @@ var modal = document.getElementById("myModal"); //Fetching the Modal window cont
 var img = document.getElementById("myImg");// Fetching the thumbnail
 var modalImg = document.getElementById("img01");//Fetching the Modal content image
 var captionText = document.getElementById("caption");//Fetching the text below the modal content
-var navigateBack=document.getElementById("naviateBack");//Fetching the nav back button
-var navigateForward=document.getElementById("naviateForward");//Fetching the nav forward button
+var navigateBack=document.getElementById("navigateBack");//Fetching the nav back button
+var navigateForward=document.getElementById("navigateForward");//Fetching the nav forward button
 var ARYthumbnails=Array.from(document.getElementsByClassName("thumbnails"));
-
+let currentIndex=null;
 // var currentIndex=;
 
 ARYthumbnails.forEach(function(thumbnail,index){   // we execute actions below onto each items in the array
 
   thumbnail.addEventListener("click",function(){    
     console.log(index);
+    currentIndex=index;
     modal.style.display = "block";// display:none からの blockにて表示
     modalImg.src = this.src;//modal imageのファイルパスをすり替えることによって画像を変える
     captionText.innerHTML = this.alt;
-
+    navigateBack.addEventListener("click",function(){
+      navBack(index);
+    });
 
   });
 
@@ -29,9 +32,16 @@ ARYthumbnails.forEach(function(thumbnail,index){   // we execute actions below o
 
 
 
-function navigateBack(){
-modalImg.src = this.src;//modal imageのファイルパスをすり替えることによって画像を変える
-captionText.innerHTML = this.alt;
+
+function navBack(index){
+  currentIndex=currentIndex-1;
+  console.log("nav back clicked!");
+  console.log("Current Index is ",currentIndex);
+  console.log("ARYthumbnails[index]=",ARYthumbnails[index]);
+  console.log(ARYthumbnails[index-1].src);
+modalImg.src = ARYthumbnails[currentIndex].src;//modal imageのファイルパスをすり替えることによって画像を変える
+captionText.innerHTML = ARYthumbnails[currentIndex].alt;
+
 
 };
 
