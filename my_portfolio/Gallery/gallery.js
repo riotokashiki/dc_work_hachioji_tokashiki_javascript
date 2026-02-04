fb81750d2168b1d2f7cd2c05a0fabce555b8ffdc
ithachioji2_546 <ithachioji21.546@gmail.com>
2026-02-04 15:53:18 +0900

    nav back and forth worked, mounted console.log () everywhere

--- my_portfolio/Gallery/gallery.js
+++ my_portfolio/Gallery/gallery.js
@@ -11,21 +11,30 @@ var ARYthumbnails=Array.from(document.getElementsByClassName("thumbnails"));
 let currentIndex=null;
 // var currentIndex=;
 
+    navigateBack.addEventListener("click",function(){
+      navBack();
+
+    });
+
+    
+    navigateForward.addEventListener("click",function(){
+      navForward();
+
+    });
+
 ARYthumbnails.forEach(function(thumbnail,index){   // we execute actions below onto each items in the array
 
   thumbnail.addEventListener("click",function(){    
-    console.log(index);
+    console.log("Photo",index,"clicked",ARYthumbnails[index]);
     currentIndex=index;
     modal.style.display = "block";// display:none からの blockにて表示
     modalImg.src = this.src;//modal imageのファイルパスをすり替えることによって画像を変える
     captionText.innerHTML = this.alt;
-    navigateBack.addEventListener("click",function(){
-      navBack(index);
-    });
-
+    
+  
   });
 
-
+    console.log(index,ARYthumbnails[index],", ready");
 
 
 });
@@ -34,17 +43,39 @@ ARYthumbnails.forEach(function(thumbnail,index){   // we execute actions below o
 
 
 function navBack(index){
+  console.log("navBack function called!"); 
+  if(currentIndex===0){
+    console.log("It has returned!");
+    return;
+  }
+  else if(currentIndex!==0){
   currentIndex=currentIndex-1;
-  console.log("nav back clicked!");
   console.log("Current Index is ",currentIndex);
-  console.log("ARYthumbnails[index]=",ARYthumbnails[index]);
-  console.log(ARYthumbnails[index-1].src);
+  console.log("ARYthumbnails[currentIndex]=",ARYthumbnails[currentIndex]);
+  
 modalImg.src = ARYthumbnails[currentIndex].src;//modal imageのファイルパスをすり替えることによって画像を変える
 captionText.innerHTML = ARYthumbnails[currentIndex].alt;
-
+}
 
 };
 
+
+function navForward(index){
+  console.log("navForward function called!"); 
+  if(currentIndex>=ARYthumbnails.length-1){
+    console.log("It has returned!");
+    return;
+  }else{
+  currentIndex=currentIndex+1;
+  console.log("Current Index is ",currentIndex);
+  console.log("ARYthumbnails[currentIndex]=",ARYthumbnails[currentIndex]);
+  
+modalImg.src = ARYthumbnails[currentIndex].src;//modal imageのファイルパスをすり替えることによって画像を変える
+console.log("The image has been replaced!");
+captionText.innerHTML = ARYthumbnails[currentIndex].alt;
+}
+
+};
 
 
 
@@ -62,4 +93,5 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
+  console.log("Modal window closed!");
 }
