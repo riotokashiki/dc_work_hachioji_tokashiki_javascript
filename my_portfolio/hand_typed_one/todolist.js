c7c08643c174e63e991f458692aef52fc71f0e80
ithachioji2_546 <ithachioji21.546@gmail.com>
2026-05-25 15:46:05 +0900

    The Code Works Perfectly Before Refactoring
    This is a state before start doing Refactoring.
    So the Recovery Point is here.

--- my_portfolio/hand_typed_one/todolist.js
+++ my_portfolio/hand_typed_one/todolist.js
@@ -4,6 +4,7 @@ let addButton=document.getElementById("addButton");
 let taskList=document.getElementById("taskList");
 
 let uncompletedList=[];
+let checkBoxArray=[];
 
 
 ///タスク追加ボタン///////////////
@@ -55,9 +56,35 @@ console.log(JSON.parse(localStorage.getItem("savedUncompletedList")));
 let checkBox=document.createElement("input");
 checkBox.type="checkbox";
 li.prepend(checkBox);
-checkBox.addEventListener("change",()=>{
-span.classList.toggle("completed")
-})
+checkBoxArray=Array.from(document.querySelectorAll("input[type='checkbox']"));
+console.log("checkBoxArray is..");
+console.log(checkBoxArray);
+
+    checkBox.addEventListener("change",(e)=>{
+        span.classList.toggle("completed");
+        let clicked_li_text=e.target.nextElementSibling.innerHTML
+        console.log("clicked_li_text is..."+clicked_li_text);
+
+        let clicked_index=uncompletedList.findIndex((item)=>{
+        return item.taskName===clicked_li_text
+        });
+        console.log("clicked_index is..."+clicked_index);
+        uncompletedList[clicked_index].completed=!uncompletedList[clicked_index].completed?true:false;
+        console.log("uncompletedList[clicked?index] is...");
+        console.log(uncompletedList[clicked_index]);
+        localStorage.setItem("savedUncompletedList",JSON.stringify(uncompletedList));
+        // let get_index= checkBoxArray.indexOf(checkBoxArray[index]);
+        // console.log("get_index is.."+get_index);
+        // uncompletedList[get_index].completed=uncompletedList[get_index].completed?false:true;
+        // console.log(uncompletedList[get_index]);
+
+          
+    })
+
+
+
+
+
 }
 
 inputField.value="";
@@ -72,54 +99,60 @@ restoreState()
 function restoreState(){
 let restoredData=JSON.parse(localStorage.getItem("savedUncompletedList"));//JSON.parseでJS配列に変換
 if(!restoredData){
-  console.log("There is no data.");
-return
+      console.log("There is no data.");
+  return
 }else{
-  uncompletedList=restoredData;//JS配列にrestoredDataを入れる　じゃないとリフレッシュすると空になってしまう
-  console.log(uncompletedList);
-restoredData.forEach((item,i)=>{
-let li=document.createElement("li");
-let span=document.createElement("span");
-span.textContent=item.taskName;
-li.appendChild(span);
-taskList.appendChild(li);
+      uncompletedList=restoredData;//JS配列にrestoredDataを入れる　じゃないとリフレッシュすると空になってしまう
+      console.log("uncompletedList=");
+      console.log(uncompletedList);
+      restoredData.forEach((item,i)=>{
+     ///li////////////////////   
+      let li=document.createElement("li");
+      let span=document.createElement("span");
+      span.textContent=item.taskName;
+      li.appendChild(span);
+      taskList.appendChild(li);
 
-///削除ボタン//////////
-let delButton=document.createElement("button");
-li.appendChild(delButton);
-delButton.innerHTML="削除";
+      ///削除ボタン//////////
+      let delButton=document.createElement("button");
+      li.appendChild(delButton);
+      delButton.innerHTML="削除";
 
-delButton.addEventListener("click",(e)=>{
-delButton.parentElement.remove();
-let clicked_li_text=e.target.previousElementSibling.innerHTML
-console.log(clicked_li_text);
-let clicked_index=uncompletedList.findIndex((item)=>{
-return item.taskName===clicked_li_text
-});
-console.log("The clicked index is.."+clicked_index);
-uncompletedList.splice(clicked_index,1);//JS配列からアイテムを削除
-console.log("Current JS array")
-console.log(uncompletedList);//アイテムが配列から削除されたことを確認
-localStorage.setItem("savedUncompletedList",JSON.stringify(uncompletedList))//localStorageを更新されたJS配列で上書き
-console.log("Current localStorage array");
-console.log(localStorage.getItem("savedUncompletedList"));
-
-
-console.log(JSON.parse(localStorage.getItem("savedUncompletedList")));
-})
+      delButton.addEventListener("click",(e)=>{
+      delButton.parentElement.remove();
+      let clicked_li_text=e.target.previousElementSibling.innerHTML
+      console.log(clicked_li_text);
+      let clicked_index=uncompletedList.findIndex((item)=>{
+      return item.taskName===clicked_li_text
+      });
+      console.log("The clicked index is.."+clicked_index);
+      uncompletedList.splice(clicked_index,1);//JS配列からアイテムを削除
+      console.log("Current JS array")
+      console.log(uncompletedList);//アイテムが配列から削除されたことを確認
+      localStorage.setItem("savedUncompletedList",JSON.stringify(uncompletedList))//localStorageを更新されたJS配列で上書き
+      console.log("Current localStorage array");
+      console.log(localStorage.getItem("savedUncompletedList"));
 
 
-///チェックボックス////////
-let checkBox=document.createElement("input");
-checkBox.type="checkbox";
-li.prepend(checkBox);
-checkBox.addEventListener("change",()=>{
-span.classList.toggle("completed")
-})
+      console.log(JSON.parse(localStorage.getItem("savedUncompletedList")));
+      })
+
+
+      ///チェックボックス////////
+      let checkBox=document.createElement("input");
+      checkBox.type="checkbox";
+      if(item.completed){
+      checkBox.checked=true;        
+      span.classList.add("completed");
+      }
+      li.prepend(checkBox);
+      checkBox.addEventListener("change",()=>{
+      span.classList.toggle("completed")
+      })
 
 
 
-})
+      })
 
 }
 
